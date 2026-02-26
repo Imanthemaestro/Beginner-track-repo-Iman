17ff5d354a3402bec8dccbcc6b298efa59fab377
Imanthemaestro <20230931@nileuniversity.edu.ng>
2026-02-26 22:36:18 +0100

    More edits to the html, css, and js of the dark mode button

--- beginner/projects/todo-list-app/src/script.js
+++ beginner/projects/todo-list-app/src/script.js
@@ -1,5 +1,13 @@
-const toggleBtn = document.getElementById('toggle-mode');
-
-toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggle-mode');
+  
+    toggleBtn.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+  
+      if (document.body.classList.contains('dark-mode')) {
+        toggleBtn.textContent = '🌞';
+      } else {
+        toggleBtn.textContent = '🌚';
+      }
+    });
+  });
